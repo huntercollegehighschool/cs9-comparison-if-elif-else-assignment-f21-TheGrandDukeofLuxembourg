119cf97c3ba1ca79a32f8e3b446aa345e0d3d151
Miles Erdly-Liang <93940204+TheGrandDukeofLuxembourg@users.noreply.github.com>
2021-12-05 22:58:24 +0000

    I finished the assignment. Thank you.-Miles Erdly-Liang

--- part3.py
+++ part3.py
@@ -20,3 +20,14 @@ not divisible by 3
 '''
 
 #write your code below
+num1=int(input("Enter a number "))
+if(num1==0):
+  print("zero")
+elif(num1<0):
+  print("negative")
+elif(num1>0):
+  print("positive")
+if(num1 % 3==0):
+  print("divisible by three")
+else:
+  print("not divisible by 3")
